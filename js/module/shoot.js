623f7453261da9ef23d20ff22e33d01e676648e9
ricardo <martinez_ric@hotmail.com>
2022-08-26 13:30:58 +0200

    fix

--- js/module/shoot.js
+++ js/module/shoot.js
@@ -11,7 +11,7 @@ function shootBullet(playerSpecs) {
       // initiate bullet create as well as animation and deletion process
       initiateBullet(playerPosition, playerSpecs, wrapper);
 
-      let shot = new Audio("./../audio/shot.mp3");
+      let shot = new Audio("./audio/shot.mp3");
 
       // shot sound
       shot.volume = 0.2;
@@ -123,7 +123,7 @@ function killEnemy(bullet, playerSpecs) {
 
         // ===== explosion
         // gif
-        enemy[i].innerHTML = `<img src="../img/explosion.gif">`;
+        enemy[i].innerHTML = `<img src="./img/explosion.gif">`;
         enemy[i].querySelector("img").style.width = "20px";
 
         // audio
